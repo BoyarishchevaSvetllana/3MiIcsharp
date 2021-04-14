300cabeaf349275ae85838de4b62733b684251f3
BoyarishchevaSvetllana <82502650+BoyarishchevaSvetllana@users.noreply.github.com>
2021-04-14 11:41:07 +0300

    Add files via upload

---
+++ lab1_1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication1{
+
+class Program{
+  
+static void Data(){
+  OperatingSystem os =
+  System.Environment.OSVersion;
+  Console.WriteLine("Platform: {0}",os.Platform);
+  System.Console.WriteLine("The current date and time is " +
+  System.DateTime.Now);
+}
+  
+static void Main(string[] args){
+  Data();
+  System.Console.ReadLine();
+}
+}
+}
